ff149088647e5a35e82809a9543d7686f0930a9f
shriDeveloper <myprojects27121997@gmail.com>
2021-01-16 23:53:04 -0800

    Added Placeholder Changes

--- shopify_app/views.py
+++ shopify_app/views.py
@@ -81,7 +81,7 @@ def logout(request):
 
 @csrf_exempt
 def submit(request):
-    API_URL = ' http://c75a5d38de7b.ngrok.io'
+    API_URL = 'http://6bf22ad3a408.ngrok.io'
     if request.method == "POST":
         font_data = request.POST.get('font_data')
         store_data = request.POST.get('store_data')
