753c5ca8929b8850be5b7d3046438502f40942bd
shriDeveloper <myprojects27121997@gmail.com>
2021-01-20 09:07:32 -0800

    Billing Removed

--- home/views.py
+++ home/views.py
@@ -16,20 +16,7 @@ def index(request):
 	try:
 		store = Store.objects.get(name=request.session['shopify']['shop_url'])
 		store_token = store.token
-	except Store.DoesNotExist:
-
-		#SET BILLING HERE
-		session = shopify.Session(shop_url, token)
-		shopify.ShopifyResource.activate_session(session)
-		charge = shopify.RecurringApplicationCharge()
-		charge.test = True
-        charge.return_url = 'https://shriCoder.pythonanywhere.com'
-        charge.price = 10.00
-        charge.name = "Custom Plan"
-
-		if charge.save():
-			print charge.attributes 
-
+	except Store.DoesNotExist: 
 		#INIIAL SETUP FOR APP
 		store = Store(name = request.session['shopify']['shop_url'] , token = store_token)
 		settings = Settings(store_token = store_token)
