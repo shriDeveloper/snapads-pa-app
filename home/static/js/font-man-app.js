b400e95ea7e89bbc8f306fbfef9512cb87dde7ab
shriDeveloper <myprojects27121997@gmail.com>
2021-03-07 20:50:58 +0000

    Files Saved

---
+++ home/static/js/font-man-app.js
@@ -0,0 +1,36 @@
+function loadFontMan(){
+      //add the link to head element
+    var fontman_link = document.createElement('link');
+      fontman_link.setAttribute('rel', 'stylesheet');
+      fontman_link.type = 'text/css';
+      fontman_link.href = 'https://www.fontman.in/static/css/font-man-picker-css.css';
+    var picker_button  = document.createElement('input');
+      picker_button.setAttribute('id', 'fontman-btn-picker');
+      picker_button.setAttribute('type', 'button');
+      picker_button.setAttribute('value', 'Picker');
+    document.head.appendChild(fontman_link);
+    document.body.appendChild(picker_button);
+    const fontman_scripts = [{
+        'id':'fontman-jquery-script',
+        'src':'https://code.jquery.com/jquery-3.6.0.min.js'
+    },{
+        'id':'fontman-element-picker',
+        'src':'https://www.fontman.in/static/js/fontman-element-picker.js'
+    },{
+        'id':'fontman-dom',
+        'src':'https://www.fontman.in/static/js/fontman-dom.js'
+    },{
+        'id':'fontman-custom-logic',
+        'src':'https://www.fontman.in/static/js/fontman-custom-logic.js'
+    }];
+    fontman_scripts.forEach((script)=>{
+      var fontman_script = document.createElement('script');  
+      fontman_script.setAttribute('id', script.id);
+      fontman_script.setAttribute('src', script.src);
+      fontman_script.setAttribute('type','text/javascript');
+      document.body.appendChild(fontman_script);
+    });  
+  }
+    
+  loadFontMan();
+  
---
+++ home/static/js/fontman-custom-logic.js
@@ -0,0 +1,35 @@
+
+    if(window.opener){
+      window.opener.postMessage({ msg: "picker",mode:'init' }, '*'),
+            window.addEventListener(
+                "message",
+                function (event) {
+                  let pick_msg = event.data.msg;
+                  let mode = event.data.mode;
+                  if(mode === 'start' && pick_msg === 'picker'){
+                      $('#fontman-btn-picker').css('display','block');
+                  }
+                  console.log('Event received');
+                },
+                !1
+            );  
+    }
+    var handlerPicker = function (element) {
+      const classes = document.getElementById('customElements');
+      const dom_path = finder(element).trim();
+      window.opener.postMessage({ msg: "picker", data: dom_path , mode:'picked' }, '*')
+      disableLinks('');
+    }
+    var myFontManPicker = DomOutline({ onClick: handlerPicker});
+    const font_picker = document.getElementById('fontman-btn-picker');
+    font_picker.addEventListener('click', function(){
+      myFontManPicker.start();
+      //disable all links on the page
+      disableLinks('none');
+    });
+    const disableLinks = (val) => {
+      const links = document.querySelectorAll('a');
+      Array.from(links).forEach( function(link) {
+        link.setAttribute('style',`pointer-events:${val}`);
+      });
+    }
